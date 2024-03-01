79d3713d4b2bf6df08c06642e23012bbd438a0f4
daoduylam2008 <96049665+daoduylam2008@users.noreply.github.com>
2024-03-01 23:12:32 +0700

    summary

--- lib/constant.dart
+++ lib/constant.dart
@@ -2,3 +2,69 @@ import 'package:flutter/material.dart';
 
 const Color accentColor = Color.fromRGBO(107, 31, 23, 1);
 const Color accentColorLow = Color.fromRGBO(107, 31, 23, .9);
+
+enum Gender { male, female }
+
+enum Users { student, teacher, admin }
+
+class Student {
+  final int id;
+  final String name;
+  final DateTime birth;
+  final String place_of_birth;
+  final Gender gender;
+  final String phone_number;
+  final String email;
+  final String parents_phone_number;
+  final String cls;
+
+  Student({
+    required this.id,
+    required this.name,
+    required this.birth,
+    required this.place_of_birth,
+    required this.gender,
+    required this.phone_number,
+    required this.email,
+    required this.parents_phone_number,
+    required this.cls,
+  });
+
+  Map<String, Object?> toMap() {
+    return {
+      'id': id,
+      "name": name,
+      "birth": birth,
+      "place of birth": place_of_birth,
+      "gender": gender,
+      "phone number": phone_number,
+      "email": email,
+      "parents phone number": parents_phone_number,
+      "class": cls,
+    };
+  }
+}
+
+class Teacher {
+  
+}
+
+class User {
+  final int id;
+  final String username;
+  final Users type;
+
+  User({
+    required this.id,
+    required this.username,
+    required this.type,
+  });
+
+  Map<String, Object?> toMap() {
+    return {
+      "id": id,
+      "username": username,
+      "type": type,
+    };
+  }
+}
