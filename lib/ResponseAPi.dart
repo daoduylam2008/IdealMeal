751af32e83e01eb88962a81ad8f79e7ddbd234c2
tiendepzai11 <sdsfdsvv@gmail.com>
2024-03-02 09:13:16 +0700

    a

--- lib/ResponseAPi.dart
+++ lib/ResponseAPi.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 import 'constant.dart';
 
-String path = 'https://jsonplaceholder.typicode.com/albums/1';
+String path = 'http://127.0.0.1:8000/api/v1/customers/151';
 
 Future<http.Response> fetchAlbum() {
   return http.get(Uri.parse(path));
