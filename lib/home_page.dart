ecaf33d8241f2ebcd9e57de8a9c0e109d99960c8
daoduylam2008 <96049665+daoduylam2008@users.noreply.github.com>
2024-03-02 12:49:31 +0700

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
+import 'DataTester.dart';
+import 'ResponseAPi.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePage();
+}
+
+class _HomePage extends State<HomePage> {
+  late Future<ImportData> futureAlbum;
+
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
 
   void pressed() {}
 
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(onPressed: pressed, child: const Text("Get data"))
-          ],
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(onPressed: pressed, child: const Text("Get data"))
+      ],
     ));
   }
 }
