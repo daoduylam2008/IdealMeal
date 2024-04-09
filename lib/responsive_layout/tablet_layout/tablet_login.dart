e61822ab40244edb4cea267f008c5373046b4765
Minh Khoi Pham <110042353+Khoipm08@users.noreply.github.com>
2024-04-09 18:15:10 +0700

    a

---
+++ lib/responsive_layout/tablet_layout/tablet_login.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
+import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool isPressed = true;
+  @override
+  Widget build(BuildContext context) {
+    double _width = MediaQuery.of(context).size.width;
+    Offset distance = isPressed ? const Offset(10, 10) : const Offset(28, 28);
+    double blur = isPressed ? 5.0 : 30.0;
+    return Scaffold(
+      backgroundColor: const Color(0xFFE7ECEF),
+      body: Center(
+        child: GestureDetector(
+          onTap: (){
+            setState(() {
+              isPressed = !isPressed;
+            });
+          },
+          child: Container(
+            width: _width/1.5,
+            height: _width/1.5,
+            decoration: BoxDecoration(
+              color: const Color(0xFFE7ECEF),
+              borderRadius: const BorderRadius.all(Radius.circular(30)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: distance,
+                  blurRadius: blur,
+                  inset: isPressed
+
+                ),
+                BoxShadow(
+                  color: Colors.white,
+                  offset: -distance,
+                  blurRadius: blur,
+                  inset: isPressed
+                ),
+              ]
+            ),
+            padding: EdgeInsets.all(100),
+            child: Column(
+              children: [
+                Text('data')
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/responsive_layout/tablet_layout/tablet_profile.dart
+++ lib/responsive_layout/tablet_layout/tablet_profile.dart
@@ -5,6 +5,12 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Profile'
+        ),
+      ),
+    );
   }
 }
