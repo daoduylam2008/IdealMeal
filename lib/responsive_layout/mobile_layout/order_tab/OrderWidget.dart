4ac8f3355e4604bc6347108e6777359225f5db33
daoduylam2008 <96049665+daoduylam2008@users.noreply.github.com>
2024-04-26 21:13:29 +0700

    summary

--- lib/responsive_layout/mobile_layout/order_tab/OrderWidget.dart
+++ lib/responsive_layout/mobile_layout/order_tab/OrderWidget.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:student_attendance/constant.dart';
 
 class OrderWidget extends StatelessWidget {
   final List<String>? lists;
+
+  final List<String> genderItems = [
+    'Male',
+    'Female',
+  ];
+  String? selectedValue;
+
   int choice = 0;
 
   OrderWidget({super.key, required this.lists});
 
   @override
   Widget build(context) {
-    return Container(
-      child: Stack(children: [
-        Text(lists![1])
-    ],));
+    return Stack(
+      children: [
+        DropdownButton(
+          borderRadius: const BorderRadius.all(Radius.circular(15)),
+          hint: Text("Select your dish", style: font(20, myGrey, FontWeight.normal)),
+          items: lists!.map((items) {
+            return DropdownMenuItem(
+                value: items, 
+                child: Text(items), 
+            );
+          }).toList(),
+          onChanged: (String? newValue) {
+            // None
+          })
+      ],
+    );
   }
 }
