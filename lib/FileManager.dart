b02f973748f4dc9db26d8c2e6ec738635c59e3ce
daoduylam2008 <96049665+daoduylam2008@users.noreply.github.com>
2024-05-01 16:44:58 +0700

    summary

--- lib/FileManager.dart
+++ lib/FileManager.dart
@@ -5,6 +5,7 @@ import 'dart:io';
 import 'package:flutter/services.dart' show rootBundle;
 
 import 'package:path_provider/path_provider.dart';
+import 'package:csv/csv.dart';
 
 Future<String> get localPath async {
   final directory = await getApplicationDocumentsDirectory();
@@ -31,3 +32,35 @@ Future<File> get userData async {
 //   // Write the file
 //   return file('$counter');
 // }
+
+Future<List<List<dynamic>>> processCsv(String path) async {
+  var result = await rootBundle.loadString(
+    path,
+  );
+
+  return const CsvToListConverter().convert(result, eol: "\n");
+}
+
+Map<String, String> mealCode(List<List<dynamic>> data) {
+  Map<String, String> map = {};
+  for (var element in data) {
+    map[element[0].split(";")[1]] = element[0].split(";")[0];
+  }
+
+  return map;
+}
+
+Map<String, List<String>> dayMeal(List<List<dynamic>> data) {
+  Map<String, List<String>> map = {};
+  for (var element1 in data) {
+    List<String> temp = [];
+    
+    for (var element2 in data) {
+      if (element1[0].split(";")[2] == element2[0].split(";")[2]) {
+        temp.add(element2[0].split(";")[1]);
+      }
+    }
+    map[element1[0].split(";")[2]] = temp;
+  }
+  return map;
+}
--- lib/responsive_layout/mobile_layout/mobile_layout.dart
+++ lib/responsive_layout/mobile_layout/mobile_layout.dart
@@ -47,7 +47,7 @@ class _MobileScaffold extends State<MobileScaffold> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: (selection == 2)? appBarBackground: Colors.white,
+      backgroundColor: Colors.white,
       appBar: (selection == 3)
           ? null
           : AppBar(
@@ -59,101 +59,101 @@ class _MobileScaffold extends State<MobileScaffold> {
                   style: font(30, Colors.black, FontWeight.bold)),
             ),
       body: IndexedStack(index: selection, children: _view ),
-
-      bottomSheet: (selection == 0) ? Container() : null,
-      // backgroundColor: Colors.white70,
-      bottomNavigationBar: LayoutBuilder(
-        builder: ((context, constraints) {
-          return SafeArea(
-              child: Stack(children: [
-            Container(
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 37, vertical: 32),
-                child: BlurryContainer(
-                  blur: 14,
-                  width: constraints.maxWidth * (368 / 430),
-                  height: 70,
-                  borderRadius: const BorderRadius.all(Radius.circular(25)),
-                  color: const Color.fromRGBO(231, 231, 231, .5),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      // Home tab
-                      InkWell(
-                        onTap: () {
-                          setState(
-                            () {
-                              selection = 0;
-                              appBarTitle = "Up Coming";
-                            },
-                          );
-                        },
-                        child: Container(
-                            padding: const EdgeInsets.all(15),
-                            child: Image.asset(
-                              iconPath("home"),
-                              color: selection == 0 ? Colors.black : null,
-                            )),
-                      ),
-
-                      // Meal tab
-                      InkWell(
-                        child: Container(
-                            padding: const EdgeInsets.all(15),
-                            child: Image.asset(
-                              iconPath("meal"),
-                              color: selection == 1 ? Colors.black : null,
-                            )),
-                        onTap: () {
-                          setState(
-                            () {
-                              selection = 1;
-                              appBarTitle = "Meal";
-                            },
-                          );
-                        },
-                      ),
-
-                      // Order tab
-                      InkWell(
-                        child: Container(
-                            padding: const EdgeInsets.all(15),
-                            child: Image.asset(
-                              iconPath("order"),
-                              color: selection == 2 ? Colors.black : null,
-                            )),
-                        onTap: () {
-                          setState(
-                            () {
-                              selection = 2;
-                              appBarTitle = "Order";
-                            },
-                          );
-                        },
-                      ),
-
-                      // Profile tab
-                      InkWell(
-                        child: Container(
-                            padding: const EdgeInsets.all(15),
-                            child: Image.asset(
-                              iconPath("profile"),
-                              color: selection == 3 ? Colors.black : null,
-                            )),
-                        onTap: () {
-                          setState(
-                            () {
-                              selection = 3;
-                              appBarTitle = "Meal";
-                            },
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                ))
-          ]));
-        }),
+      
+      bottomNavigationBar: SizedBox(
+        child: LayoutBuilder(
+          builder: ((context, constraints) {
+            return SafeArea(
+                child: Stack(children: [
+              Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 37, vertical: 32),
+                  child: BlurryContainer(
+                    blur: 14,
+                    width: constraints.maxWidth * (368 / 430),
+                    height: 70,
+                    borderRadius: const BorderRadius.all(Radius.circular(25)),
+                    color: const Color.fromRGBO(231, 231, 231, .5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        // Home tab
+                        InkWell(
+                          onTap: () {
+                            setState(
+                              () {
+                                selection = 0;
+                                appBarTitle = "Up Coming";
+                              },
+                            );
+                          },
+                          child: Container(
+                              padding: const EdgeInsets.all(15),
+                              child: Image.asset(
+                                iconPath("home"),
+                                color: selection == 0 ? Colors.black : null,
+                              )),
+                        ),
+        
+                        // Meal tab
+                        InkWell(
+                          child: Container(
+                              padding: const EdgeInsets.all(15),
+                              child: Image.asset(
+                                iconPath("meal"),
+                                color: selection == 1 ? Colors.black : null,
+                              )),
+                          onTap: () {
+                            setState(
+                              () {
+                                selection = 1;
+                                appBarTitle = "Meal";
+                              },
+                            );
+                          },
+                        ),
+        
+                        // Order tab
+                        InkWell(
+                          child: Container(
+                              padding: const EdgeInsets.all(15),
+                              child: Image.asset(
+                                iconPath("order"),
+                                color: selection == 2 ? Colors.black : null,
+                              )),
+                          onTap: () {
+                            setState(
+                              () {
+                                selection = 2;
+                                appBarTitle = "Order";
+                              },
+                            );
+                          },
+                        ),
+        
+                        // Profile tab
+                        InkWell(
+                          child: Container(
+                              padding: const EdgeInsets.all(15),
+                              child: Image.asset(
+                                iconPath("profile"),
+                                color: selection == 3 ? Colors.black : null,
+                              )),
+                          onTap: () {
+                            setState(
+                              () {
+                                selection = 3;
+                                appBarTitle = "Meal";
+                              },
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ))
+            ]));
+          }),
+        ),
       ),
     );
   }
