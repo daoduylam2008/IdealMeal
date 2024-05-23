bfddc6fa328ccf21c045a4741598c37701a37450
tiendepzai11 <sdsfdsvv@gmail.com>
2024-05-23 21:32:14 +0700

    idk

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/usr/local/Caskroom/flutter/3.16.8/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/daoduyluu/Mobile Development/ideal_meal"
+export "FLUTTER_ROOT=/Users/tienle/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/tienle/Flutter-project/GitHub/IdealMeal"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
