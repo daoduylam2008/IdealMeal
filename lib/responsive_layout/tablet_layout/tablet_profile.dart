c7f92673a66aea1dda88e909d87654e02667f729
daoduylam2008 <96049665+daoduylam2008@users.noreply.github.com>
2024-07-16 20:41:29 +0700

    update tablet

--- lib/responsive_layout/tablet_layout/tablet_profile.dart
+++ lib/responsive_layout/tablet_layout/tablet_profile.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+final kToday = DateTime.now();
+final kFirstDay = DateTime(kToday.year, kToday.month - 3, kToday.day);
+final kLastDay = DateTime(kToday.year, kToday.month + 3, kToday.day);
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
+  final CalendarFormat _calendarFormat = CalendarFormat.month;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           'Profile'
         ),
       ),
+      body: Column(children: [
+        TableCalendar(
+          calendarFormat: _calendarFormat,
+          focusedDay: DateTime.now(),
+           firstDay: kFirstDay,
+            lastDay: kLastDay
+            )
+      ],),
     );
   }
 }
