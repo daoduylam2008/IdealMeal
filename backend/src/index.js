da94047505f4851d3f438f5993a49ad1379e26d9
Minh Khoi Pham <110042353+Khoipm08@users.noreply.github.com>
2024-07-22 12:27:12 +0700

    ass

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const route = require("./routes");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
+var cors = require("cors");
 
 const app = express();
 const port = 1111;
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.listen(port, () => {
   console.log(`- Local: http://localhost:${port}`);
 });
 
 const middleware = (req, res, next) => {
-  const token = req.cookies.token
+  const token = req.cookies.token;
   next();
 };
 app.use(middleware);
